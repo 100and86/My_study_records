9adba7fffd9cb9f751c4522c7e013d3118793919
100and86 <1743357997@qq.com>
2026-07-11 20:41:28 +0800

    XX

--- src/ch23/clock2/main.go
+++ src/ch23/clock2/main.go
@@ -1,1 +1,52 @@
 package main
+
+import (
+	"flag"
+	"io"
+	"log"
+	"net"
+	"time"
+)
+
+func main() {
+	var port string
+	var zone string
+	flag.StringVar(&port, "port", "8000", "服务器监听")
+	flag.StringVar(&zone, "zone", "Local", "时区，例如 Asia/Tokyo")
+	flag.Parse()
+
+	address := "127.0.0.1:" + port
+	location, err := time.LoadLocation(zone)
+	if err != nil {
+		log.Fatalf("无效时区 %q：%v", zone, err)
+	}
+	listener, err := net.Listen("tcp", address)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer listener.Close()
+
+	log.Printf("监听%s端口中,时区为%s", port, zone)
+
+	for {
+		conn, err := listener.Accept() //阻塞时调用
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go handleConn(conn, location)
+
+	}
+
+}
+
+func handleConn(conn net.Conn,location *time.Location) {
+	defer conn.Close()
+	for {
+		_, err := io.WriteString(conn, time.Now().In(location).Format("15:04:05\n"))
+		if err != nil {
+			return
+		}
+		time.Sleep(time.Second)
+	}
+}
--- src/ch23/clockwall/main.go
+++ src/ch23/clockwall/main.go
@@ -1,1 +1,109 @@
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+	"time"
+)
+
+type Clock struct {
+	Name    string
+	Address string
+}
+type Update struct {
+	Name  string
+	Value string
+}
+
+func main() {
+	if len(os.Args[1:]) == 0 {
+		log.Fatal(
+			"请传入时钟服务器，例如：" +
+				" NewYork=127.0.0.1:8010",
+		)
+	}
+	updates := make(chan Update)
+	clocks := make([]Clock, 3, 5)
+	for _, argument := range os.Args[1:] {
+		clock, err := parseClock(argument)
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+		go readClock(clock, updates)
+		clocks = append(clocks, clock)
+		fmt.Printf("%-15s", clock.Name)
+	}
+	fmt.Println()
+	latest := make(map[string]string)
+
+	ticker := time.NewTicker(time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case update := <-updates:
+			latest[update.Name] = update.Value
+
+		case <-ticker.C:
+			printTimes(clocks, latest)
+		}
+	}
+
+}
+
+func readClock(clock Clock, updates chan<- Update) {
+	conn, err := net.Dial("tcp", clock.Address)
+	if err != nil {
+		updates <- Update{
+			Name:  clock.Name,
+			Value: "连接失败",
+		}
+	}
+	defer conn.Close()
+	scanner := bufio.NewScanner(conn)
+	for scanner.Scan() {
+		updates <- Update{
+			Name:  clock.Name,
+			Value: scanner.Text(),
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		updates <- Update{
+			Name:  clock.Name,
+			Value: "读取失败",
+		}
+	}
+
+}
+
+func parseClock(argument string) (Clock, error) {
+	name, address, found := strings.Cut(argument, "=")
+	if !found || name == "" || address == "" {
+		return Clock{}, fmt.Errorf(
+			"参数格式错误：%q，应为 名称=地址",
+			argument,
+		)
+	}
+
+	return Clock{
+		Name:    name,
+		Address: address,
+	}, nil
+}
+func printTimes(clocks []Clock, latest map[string]string) {
+	for _, clock := range clocks {
+		value := latest[clock.Name]
+		if value == "" {
+			value = "--:--:--"
+		}
+
+		fmt.Printf("%-15s", value)
+	}
+
+	fmt.Println()
+}
